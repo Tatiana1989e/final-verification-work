41dac358a257de694ddaa4a93aa76ecbb269722f
Egorova Tatiana <tanyasha.89@mail.ru>
2022-09-16 21:58:00 +0300

    finalverificationwork2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
-Console.WriteLine($"Введите информацию: ");
+Console.WriteLine("Введите информацию: ");
 string[] array1 = new string[3];
 string[] array2 = new string[array1.Length];
+int current = 0;
 void NewArray(string[] array1, string[] array2)
 {
+    int current = 0;
     for (int i = 0; i < array1.Length; i++)
     {
     if(array1[i].Length <= 3);
